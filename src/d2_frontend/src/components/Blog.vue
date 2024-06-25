069b252255f8b95b6bc7e3cfe2e4f3ff0e975a07
DewNawtNew <pan.treser1@wp.pl>
2024-06-25 19:44:20 +0200

    zmiana pierwsza, dodany blog

---
+++ src/d2_frontend/src/components/Blog.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
+        {{ wpisy }}
+    </div>
+</template>
+
+<script>
+import { d2_backend } from 'declarations/d2_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            this.wpisy = await d2_backend.odczytaj_wpisy();
+        }
+    },
+}
+</script>
