2ec401f34799835d36a76dae7cb2fef865f5e781
DewNawtNew <pan.treser1@wp.pl>
2024-06-26 19:11:44 +0200

    skonczony, Blog, tailwind dodany

--- src/d2_frontend/src/components/Blog.vue
+++ src/d2_frontend/src/components/Blog.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-color-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4 p-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-100">
+                <p>{{wpis}}</p>
+            </div>
+            <div class="flex justify-center flex-col">
+                <input v-model="nowyBlog" type="text">
+                <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
+            </div>
+        </div>
     </div>
 </template>
-
 <script>
 import { d2_backend } from 'declarations/d2_backend/index';
 
@@ -26,6 +33,8 @@ export default {
             this.wpisy = await d2_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted(){
+        this.pobierzWpisy();
+    }
 }
 </script>
